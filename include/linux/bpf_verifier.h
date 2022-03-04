56c5d1abfa0d813ef062353f538617ca8f89a555
Kumar Kartikeya Dwivedi <memxor@gmail.com>
2022-03-05 04:16:38 +0530

    UPSTREAM: bpf: Add check_func_arg_reg_off function
    Lift the list of register types allowed for having fixed and variable
    offsets when passed as helper function arguments into a common helper,
    so that they can be reused for kfunc checks in later commits. Keeping a
    common helper aids maintainability and allows us to follow the same
    consistent rules across helpers and kfuncs. Also, convert check_func_arg
    to use this function.
    
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20220304224645.3677453-2-memxor@gmail.com

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -547,6 +547,9 @@ bpf_prog_offload_replace_insn(struct bpf_verifier_env *env, u32 off,
 void
 bpf_prog_offload_remove_insns(struct bpf_verifier_env *env, u32 off, u32 cnt);
 
+int check_func_arg_reg_off(struct bpf_verifier_env *env,
+			   const struct bpf_reg_state *reg, int regno,
+			   enum bpf_arg_type arg_type);
 int check_kfunc_mem_size_reg(struct bpf_verifier_env *env, struct bpf_reg_state *reg,
 			     u32 regno);
 int check_ptr_off_reg(struct bpf_verifier_env *env,
