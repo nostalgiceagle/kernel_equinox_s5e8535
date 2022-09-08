1beb195bedab2dd4c477b82ea23f5df3f0bc5b47
Dave Marchevsky <davemarchevsky@fb.com>
2022-09-08 16:07:16 -0700

    UPSTREAM: bpf: Add verifier support for custom callback return range
    Verifier logic to confirm that a callback function returns 0 or 1 was
    added in commit 69c087ba6225b ("bpf: Add bpf_for_each_map_elem() helper").
    At the time, callback return value was only used to continue or stop
    iteration.
    
    In order to support callbacks with a broader return value range, such as
    those added in rbtree series[0] and others, add a callback_ret_range to
    bpf_func_state. Verifier's helpers which set in_callback_fn will also
    set the new field, which the verifier will later use to check return
    value bounds.
    
    Default to tnum_range(0, 0) instead of using tnum_unknown as a sentinel
    value as the latter would prevent the valid range (0, U64_MAX) being
    used. Previous global default tnum_range(0, 1) is explicitly set for
    extant callback helpers. The change to global default was made after
    discussion around this patch in rbtree series [1], goal here is to make
    it more obvious that callback_ret_range should be explicitly set.
    
      [0]: lore.kernel.org/bpf/20220830172759.4069786-1-davemarchevsky@fb.com/
      [1]: lore.kernel.org/bpf/20220830172759.4069786-2-davemarchevsky@fb.com/
    
    Signed-off-by: Dave Marchevsky <davemarchevsky@fb.com>
    Reviewed-by: Stanislav Fomichev <sdf@google.com>
    Link: https://lore.kernel.org/r/20220908230716.2751723-1-davemarchevsky@fb.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -1761,6 +1761,7 @@ static void init_func_state(struct bpf_verifier_env *env,
 	state->callsite = callsite;
 	state->frameno = frameno;
 	state->subprogno = subprogno;
+	state->callback_ret_range = tnum_range(0, 0);
 	init_reg_state(env, state);
 	mark_verifier_state_scratched(env);
 }
@@ -7062,6 +7063,7 @@ static int set_map_elem_callback_state(struct bpf_verifier_env *env,
 		return err;
 
 	callee->in_callback_fn = true;
+	callee->callback_ret_range = tnum_range(0, 1);
 	return 0;
 }
 
@@ -7083,6 +7085,7 @@ static int set_loop_callback_state(struct bpf_verifier_env *env,
 	__mark_reg_not_init(env, &callee->regs[BPF_REG_5]);
 
 	callee->in_callback_fn = true;
+	callee->callback_ret_range = tnum_range(0, 1);
 	return 0;
 }
 
@@ -7112,6 +7115,7 @@ static int set_timer_callback_state(struct bpf_verifier_env *env,
 	__mark_reg_not_init(env, &callee->regs[BPF_REG_4]);
 	__mark_reg_not_init(env, &callee->regs[BPF_REG_5]);
 	callee->in_async_callback_fn = true;
+	callee->callback_ret_range = tnum_range(0, 1);
 	return 0;
 }
 
@@ -7139,6 +7143,7 @@ static int set_find_vma_callback_state(struct bpf_verifier_env *env,
 	__mark_reg_not_init(env, &callee->regs[BPF_REG_4]);
 	__mark_reg_not_init(env, &callee->regs[BPF_REG_5]);
 	callee->in_callback_fn = true;
+	callee->callback_ret_range = tnum_range(0, 1);
 	return 0;
 }
 
@@ -7165,7 +7170,7 @@ static int prepare_func_exit(struct bpf_verifier_env *env, int *insn_idx)
 	caller = state->frame[state->curframe - 1];
 	if (callee->in_callback_fn) {
 		/* enforce R0 return value range [0, 1]. */
-		struct tnum range = tnum_range(0, 1);
+		struct tnum range = callee->callback_ret_range;
 
 		if (r0->type != SCALAR_VALUE) {
 			verbose(env, "R0 not a scalar value\n");
