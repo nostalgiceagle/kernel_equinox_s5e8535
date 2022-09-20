9fba1caf8a0b0e8ff1c5d0e9fb5ca7bd707b83e8
Roberto Sassu <roberto.sassu@huawei.com>
2022-09-20 09:59:42 +0200

    UPSTREAM: btf: Allow dynamic pointer parameters in kfuncs
    Allow dynamic pointers (struct bpf_dynptr_kern *) to be specified as
    parameters in kfuncs. Also, ensure that dynamic pointers passed as argument
    are valid and initialized, are a pointer to the stack, and of the type
    local. More dynamic pointer types can be supported in the future.
    
    To properly detect whether a parameter is of the desired type, introduce
    the stringify_struct() macro to compare the returned structure name with
    the desired name. In addition, protect against structure renames, by
    halting the build with BUILD_BUG_ON(), so that developers have to revisit
    the code.
    
    To check if a dynamic pointer passed to the kfunc is valid and initialized,
    and if its type is local, export the existing functions
    is_dynptr_reg_valid_init() and is_dynptr_type_expected().
    
    Cc: Joanne Koong <joannelkoong@gmail.com>
    Cc: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Signed-off-by: Roberto Sassu <roberto.sassu@huawei.com>
    Acked-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Link: https://lore.kernel.org/r/20220920075951.929132-5-roberto.sassu@huaweicloud.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: nostalgiceagle <183935403+nostalgiceagle@users.noreply.github.com>

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -600,6 +600,11 @@ int check_ptr_off_reg(struct bpf_verifier_env *env,
 		      const struct bpf_reg_state *reg, int regno);
 int check_mem_reg(struct bpf_verifier_env *env, struct bpf_reg_state *reg,
 		   u32 regno, u32 mem_size);
+bool is_dynptr_reg_valid_init(struct bpf_verifier_env *env,
+			      struct bpf_reg_state *reg);
+bool is_dynptr_type_expected(struct bpf_verifier_env *env,
+			     struct bpf_reg_state *reg,
+			     enum bpf_arg_type arg_type);
 
 /* this lives here instead of in bpf.h because it needs to dereference tgt_prog */
 static inline u64 bpf_trampoline_compute_key(const struct bpf_prog *tgt_prog,
